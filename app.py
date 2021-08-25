5f297f56ec193ced2391a1b0259b18f59b82852d
unknown <iden.ticlla@gmail.com>
2021-08-25 14:13:22 -0400

    database setup

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from fastapi import FastAPI
+import databases, sqlalchemy
 
+
+# Postgres database
+DATABASE_URL = "postgresql://usertest:usertest222@127.0.0.1:5432/dbtest"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+users = sqlalchemy.Table(
+    "py_users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("username", sqlalchemy.String),
+    sqlalchemy.Column("password", sqlalchemy.String),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+    sqlalchemy.Column("gender", sqlalchemy.CHAR),
+    sqlalchemy.Column("created_at", sqlalchemy.String),
+    sqlalchemy.Column("status", sqlalchemy.CHAR),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+
+metadata.create_all(engine)
 
 app = FastAPI()
 
